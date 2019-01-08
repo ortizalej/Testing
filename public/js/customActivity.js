3feaca4afce221c464c89a8a0752f6a545062863
unknown <alejandro.ortiz@xappia.com>
2019-01-08 16:07:16 -0300

    test

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -31,6 +31,7 @@ define([
             for(var i = 0; i < data['schema'].length; i++) {
                 var split = data['schema'][i].key.split('.');
                 console.log(split)
+                console.log(split[2])
                 if(split[2] === 'CampaignMember:Common:MobilePhone'){
                     phone = data['schema'][i].key;
                } else if(split[2] === 'firstName'){
